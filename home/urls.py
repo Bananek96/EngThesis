cda83d418caceab37e0098d6ae43ae803abee608
Bananek96 <Banasiak_Jakub@wp.pl>
2023-10-11 12:13:35 +0200

    Template in eng & pl for website

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'home'
-urlpatterns = [
-    path('', views.index_pl, name='index_pl'),
-    path('en', views.index_en, name='index_en'),
-]
